dacaa8e6c28c1956ed3700286e65a88d4ea8a027
dom-kom12 <komputer200012@gmail.com>
2025-05-03 11:12:41 +0200

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.getElementById('header');
+    const footer = document.getElementById('footer');
+
+    // Dynamiczne ładowanie komponentów
+    header.innerHTML = '<h1>Strona Główna</h1>';
+    footer.innerHTML = '<p>© 2025 Moja Strona</p>';
+
+    // Przykładowa interakcja
+    console.log("WEBPAGE IS START");
+});
