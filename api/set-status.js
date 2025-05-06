f628d85a4a29e98764e460889506933e0212b1de
dom-kom12 <komputer200012@gmail.com>
2025-05-06 17:46:31 +0200

    Update set-status.js

--- api/set-status.js
+++ api/set-status.js
@@ -1,17 +1,12 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method === 'POST') {
-    const { message } = req.body;
-
-    // Możesz tu np. dodać zabezpieczenie hasłem
-    if (req.headers.authorization !== 'domino2012r') {
-      return res.status(403).json({ error: 'Brak dostępu' });
+    const body = req.body;
+    if (!body.status) {
+      return res.status(400).json({ error: 'Brak statusu' });
     }
-
-    // Zamiast pliku – Redis, Supabase, Upstash, etc.
-    globalThis.status = message;
-    return res.status(200).json({ success: true });
+    // tu ustaw status
+    return res.status(200).json({ success: true, status: body.status });
+  } else {
+    return res.status(405).json({ error: 'Zła metoda' });
   }
-
-  res.status(405).end();
 }
-
